029bf06daf7d05ef62f625171b5edb2c871a2b0a
leonid-shutov <leonid.shutov.main@gmail.com>
2023-12-02 19:04:04 +0200

    improve types

--- meta-class.d.ts
+++ meta-class.d.ts
@@ -3,12 +3,12 @@ import { ZodObject, ZodRawShape, infer as zinfer } from "zod";
 export type TStrategyOutput<TMetaClass> = { metaClass: TMetaClass; metaData: { keys: string[] } };
 
 export type TMetaClass = {
-  fromKeys: <IDefault = any, I extends IDefault = IDefault>(
-    keys: (keyof I)[]
-  ) => TStrategyOutput<new (argsObj: I) => I>;
-  fromZodSchema: <T extends ZodRawShape>(
-    schema: ZodObject<T>
-  ) => TStrategyOutput<new (argsObj: zinfer<typeof schema>) => zinfer<typeof schema>>;
+  fromKeys: <TMetaKeys>(
+    keys: (keyof TMetaKeys)[]
+  ) => TStrategyOutput<new <TDomainModel extends TMetaKeys>(argsObj: TDomainModel) => TDomainModel>;
+  fromZodSchema: <TMetaSchema extends ZodRawShape>(
+    schema: ZodObject<TMetaSchema>
+  ) => TStrategyOutput<new <TDomainModel extends zinfer<typeof schema>>(argsObj: TDomainModel) => TDomainModel>;
 };
 
 export const MetaClass: TMetaClass;
