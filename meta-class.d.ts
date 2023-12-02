3fffd7c75b28aea46dd8fe187da64c30fb98f6df
leonid-shutov <leonid.shutov.main@gmail.com>
2023-12-02 19:23:36 +0200

    improve naming

--- meta-class.d.ts
+++ meta-class.d.ts
@@ -1,14 +1,23 @@
 import { ZodObject, ZodRawShape, infer as zinfer } from "zod";
 
-export type TStrategyOutput<TMetaClass> = { metaClass: TMetaClass; metaData: { keys: string[] } };
+export type TStrategyOutput<TMetaClass> = {
+  metaClass: TMetaClass;
+  metaData: { keys: string[] };
+};
 
 export type TMetaClass = {
-  fromKeys: <TMetaKeys>(
-    keys: (keyof TMetaKeys)[]
-  ) => TStrategyOutput<new <TDomainModel extends TMetaKeys>(argsObj: TDomainModel) => TDomainModel>;
+  fromKeys: <TMetaModel>(
+    keys: (keyof TMetaModel)[]
+  ) => TStrategyOutput<
+    new <TDomainModel extends TMetaModel>(argsObj: TDomainModel) => TDomainModel
+  >;
   fromZodSchema: <TMetaSchema extends ZodRawShape>(
     schema: ZodObject<TMetaSchema>
-  ) => TStrategyOutput<new <TDomainModel extends zinfer<typeof schema>>(argsObj: TDomainModel) => TDomainModel>;
+  ) => TStrategyOutput<
+    new <TDomainModel extends zinfer<typeof schema>>(
+      argsObj: TDomainModel
+    ) => TDomainModel
+  >;
 };
 
 export const MetaClass: TMetaClass;
